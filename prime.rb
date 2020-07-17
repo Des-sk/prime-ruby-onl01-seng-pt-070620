736e2277433a4f261b32b98da16e8ee243ba2404
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-17 15:09:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,3 +8,19 @@ def prime?(num)
  
 end
 
+def prime?(number)
+  if !number.integer?
+    return false
+  elsif number < 2
+    return false
+  elsif number == 2
+    return true
+  elsif (2...number - 1).each do |int|
+      if number % int == 0
+        return false
+      end
+    end
+  else
+     return true    
+  end  
+end
